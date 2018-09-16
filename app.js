7d4140eff9eb8837f53fa0b578c16e30d912c70b
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2018-09-16 17:40:45 +0530

    added middleware to extend heroku timeout issue

--- app.js
+++ app.js
@@ -62,6 +62,61 @@ var transform = {
     ]
 };
 
+//Code to extend time in heroku response
+const extendTimeoutMiddleware = (req, res, next) => {
+    const space = ' ';
+    let isFinished = false;
+    let isDataSent = false;
+
+    // Only extend the timeout for API requests
+    if (!req.url.includes('/')) {
+        next();
+        return;
+    }
+
+    res.once('finish', () => {
+        isFinished = true;
+    });
+
+    res.once('end', () => {
+        isFinished = true;
+    });
+
+    res.once('close', () => {
+        isFinished = true;
+    });
+
+    res.on('data', (data) => {
+        // Look for something other than our blank space to indicate that real
+        // data is now being sent back to the client.
+        if (data !== space) {
+            isDataSent = true;
+        }
+    });
+
+    const waitAndSend = () => {
+        setTimeout(() => {
+            // If the response hasn't finished and hasn't sent any data back....
+            if (!isFinished && !isDataSent) {
+                // Need to write the status code/headers if they haven't been sent yet.
+                if (!res.headersSent) {
+                    res.writeHead(202);
+                }
+
+                res.write(space);
+
+                // Wait another 15 seconds
+                waitAndSend();
+            }
+        }, 15000);
+    };
+
+    waitAndSend();
+    next();
+};
+
+
+//end of code to extend time in heroku server
 
 module.exports = function () {
     var self = this;
@@ -70,6 +125,7 @@ module.exports = function () {
         var app = express();
         var router = express.Router();
         router.use(bodyParser.json());
+        app.use(extendTimeoutMiddleware);
         app.use('/', router);
         var logger = (config ? config.logger : null);
         if (!logger) {
